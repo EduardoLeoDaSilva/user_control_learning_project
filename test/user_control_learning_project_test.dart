c7afffd1d3e5a16c72403e62ffbe3f626b8b45d0
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-21 19:45:35 -0300

    Iniciando projeto

---
+++ test/user_control_learning_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:user_control_learning_project/user_control_learning_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
