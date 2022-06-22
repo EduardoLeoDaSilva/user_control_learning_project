c90853de6b2e015dbe620d6b29731daa28d33098
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-22 05:52:20 -0300

    Custom exception created

---
+++ lib/exceptions/userException.dart
@@ -0,0 +1,7 @@
+class UserException implements Exception {
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Opereção com o usuário não efetuada com sucesso.';
+  }
+}
